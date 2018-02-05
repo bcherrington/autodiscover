54de99a67bf0ad8f35acdf6fe4f1a7bafed83c00
Bruce Cherrington <bruce@chronologic.co.za>
2018-02-05 16:26:41 +0200

    Create separate file to handle default values

--- lib/autodiscover.rb
+++ lib/autodiscover.rb
@@ -4,8 +4,28 @@ require 'nori'
 require 'httpclient'
 require 'logging'
 
+#require 'debug'
+
 module Autodiscover
   Logging.logger['Autodiscover'].level = :info
+
+  # Logging.logger.root.level = :debug
+  # Logging.color_scheme('bright',
+  #                      lines: {
+  #                          debug: :black,
+  #                          info:  :bright_blue,
+  #                          warn:  :yellow,
+  #                          error: :red,
+  #                          fatal: [:white, :on_red] })
+  #
+  # layout = Logging.layouts.pattern pattern:      "%d %-#{::Logging::MAX_LEVEL_LENGTH}l: [%c] %m\n",
+  #                                  date_pattern: '%d-%m-%Y %H:%M:%S.%s',
+  #                                  color_scheme: 'bright'
+  # # Logging.logger.root.appenders = Logging.appenders.stdout layout: layout
+  # Logging.logger.root.appenders = Logging.appenders.file('logs/autodiscover.log', layout: layout)
+  #
+  # Logging.logger.root.level                = :debug
+  # Logging.logger['Autodiscover'].level     = :debug
 
   def self.logger
     Logging.logger['Autodiscover']
@@ -16,6 +36,7 @@ module Autodiscover
   end
 end
 
+require 'autodiscover/defaults'
 require 'autodiscover/errors'
 require 'autodiscover/client'
 require 'autodiscover/pox_request'
---
+++ lib/autodiscover/defaults.rb
@@ -0,0 +1,3 @@
+module Autodiscover
+  CA_BUNDLE = %w[/etc/pki/tls/certs/ca-bundle.crt /etc/pki/tls/certs/ca-bundle.trust.crt]
+end
