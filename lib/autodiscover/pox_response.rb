5d2dc6fd00f3ff7dadaa97d90ac389359d1c9f15
Bruce Cherrington <bruce@chronologic.co.za>
2018-02-05 16:23:35 +0200

    Added default empty or blank values should a parameter not exist in the response class.

--- lib/autodiscover/pox_response.rb
+++ lib/autodiscover/pox_response.rb
@@ -1,19 +1,18 @@
 module Autodiscover
   class PoxResponse
-
     attr_reader :response
 
     def initialize(response)
-      raise ArgumentError, 'Response must be an XML string' if(response.nil? || response.empty?)
+      raise Autodiscover::ArgumentError, 'Response must be an XML string' if response.nil? || response.empty?
       @response = Nori.new(parser: :nokogiri).parse(response)['Autodiscover']['Response']
     end
 
-    def user_displayname
+    def user_display_name
       @display_name ||= user['DisplayName'] || {}
     end
 
     def user
-      response['User']
+      response['User'] || {}
     end
 
     def exchange_version
@@ -41,27 +40,27 @@ module Autodiscover
     end
 
     def exch_proto
-      @exch_proto ||= (account['Protocol'].select{|p| p['Type'] == 'EXCH'}.first || {})
+      @exch_proto ||= (account['Protocol'].select { |p| p['Type'] == 'EXCH' }.first || {})
     end
 
     def exhttp_proto
-      @exhttp_proto ||= (account['Protocol'].select{|p| p['Type'] == 'EXHTTP'}.first || {})
+      @exhttp_proto ||= (account['Protocol'].select { |p| p['Type'] == 'EXHTTP' }.first || {})
     end
 
     def expr_proto
-      @expr_proto ||= (account['Protocol'].select{|p| p['Type'] == 'EXPR'}.first || {})
+      @expr_proto ||= (account['Protocol'].select { |p| p['Type'] == 'EXPR' }.first || {})
     end
 
     def web_proto
-      @web_proto ||= (account['Protocol'].select{|p| p['Type'] == 'WEB'}.first || {})
+      @web_proto ||= (account['Protocol'].select { |p| p['Type'] == 'WEB' }.first || {})
     end
 
     def redirect_addr
-      account['RedirectAddr']
+      account['RedirectAddr'] || ''
     end
 
     def redirect_url
-      account['RedirectUrl']
+      account['RedirectUrl'] || ''
     end
 
     def account
