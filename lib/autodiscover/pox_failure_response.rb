f0635e07331383aa74249deefc9c4828d7a2ad3f
Laurence Hardman <hardmanlaurence@gmail.com>
2018-02-13 09:15:25 +0200

    Integration testing for failure due to incorrect credentials, and unknown host

--- lib/autodiscover/pox_failure_response.rb
+++ lib/autodiscover/pox_failure_response.rb
@@ -5,6 +5,11 @@ module Autodiscover
     def initialize(code, message)
       @code = code
       @message = message
+      @logger = Logging.logger[self.class.name]
+    end
+
+    def dump_details
+      @logger.info "PoxFailedResponse: Code #{@code.to_s}, #{@message.to_s}"
     end
 
   end
--- spec/integration tests/complete_integration_spec.rb
+++ spec/integration tests/complete_integration_spec.rb
@@ -1,6 +1,8 @@
 require 'autodiscover'
 require 'spec_helper'
 
+@logger = Logging.logger['Integration testing']
+
 feature 'User attempts to discover' do
   scenario 'when all details are correct' do
 
@@ -9,16 +11,34 @@ feature 'User attempts to discover' do
 
     client = Autodiscover::Client.new(email: email, password: password)
     data = client.autodiscover
+    data.dump_details
 
     expect(data).to be_a(Autodiscover::PoxResponse)
     expect(data).not_to be_a(Autodiscover::PoxFailedResponse)
   end
 
   scenario 'with incorrect credentials' do
+    email = 'chronosync@chronologic.co.za'
+    password = 'something_incorrect'
+
+    client = Autodiscover::Client.new(email: email, password: password)
+    data = client.autodiscover
+    data.dump_details
+
+    expect(data).to be_a(Autodiscover::PoxFailedResponse)
 
   end
 
   scenario 'to unknown host' do
+    email = 'chronosync@chronologic.co.za'
+    password = 'something'
+
+    client = Autodiscover::Client.new(email: email, password: password, domain: 'chronologicunknown.co.za')
+    data = client.autodiscover
+    data.dump_details
+
+    expect(data).to be_a(Autodiscover::PoxFailedResponse) and expect(data.message.include?('Unknown host')).to be true
+
 
   end
 
